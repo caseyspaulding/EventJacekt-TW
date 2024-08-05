1ddbee22ab108d4b06cce4ba4f1b109d1a18283a
Casey Spaulding <cspau17@wgu.edu>
2024-08-05 19:21:32 -0400

    more updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import CTACentered from '@/components/CTA/CTA-Centered';
-import FeaturesGridWithIcons from '@/components/Features/FeaturesGridwithIcons';
-import FooterTW from '@/components/Footers/FooterTW';
-import NavBarTW from '@/components/NavBarTW/NavBarTW';
+import CTACentered from "@/components/CTA/CTA-Centered";
+import FeaturesGridWithIcons from "@/components/Features/FeaturesGridwithIcons";
+import FooterTW from "@/components/Footers/FooterTW";
+import NavBarTW from "@/components/NavBarTW/NavBarTW";
 
 export default async function Index() {
-    return (
-        <>
-            <div className="bg-white ">
-                <NavBarTW />
-                <FeaturesGridWithIcons />
-                <CTACentered />
-                <FooterTW />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <NavBarTW />
+      <FeaturesGridWithIcons />
+      <CTACentered />
+      <FooterTW />
+    </>
+  );
 }
--- components/NavBarTW/NavBarTW.tsx
+++ components/NavBarTW/NavBarTW.tsx
@@ -1,38 +1,40 @@
 "use client";
 
-import {
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
-} from "@headlessui/react";
+import
+  {
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
+  } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import {
-  Bars3Icon,
-  ChatBubbleBottomCenterTextIcon,
-  ChatBubbleLeftRightIcon,
-  InboxIcon,
-  QuestionMarkCircleIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
+import
+  {
+    Bars3Icon,
+    ChatBubbleBottomCenterTextIcon,
+    ChatBubbleLeftRightIcon,
+    InboxIcon,
+    QuestionMarkCircleIcon,
+    XMarkIcon,
+  } from "@heroicons/react/24/outline";
 
 const solutions = [
   {
-    name: "Inbox",
+    name: "Ticketing",
     description:
       "Get a better understanding of where your traffic is coming from.",
     href: "#",
     icon: InboxIcon,
   },
   {
-    name: "Messaging",
-    description: "Speak directly to your customers in a more meaningful way.",
+    name: "CRM",
+    description: "Build and Maintain your relationships.",
     href: "#",
     icon: ChatBubbleBottomCenterTextIcon,
   },
   {
     name: "Live Chat",
-    description: "Your customers' data will be safe and secure.",
+    description: "Organize your team's workflow and get more done.",
     href: "#",
     icon: ChatBubbleLeftRightIcon,
   },
@@ -49,7 +51,8 @@ const navigation = [
   { name: "Company", href: "#" },
 ];
 
-export default function NavBarTW() {
+export default function NavBarTW ()
+{
   return (
     <div className="bg-white">
       <header>
@@ -63,7 +66,7 @@ export default function NavBarTW() {
                   src="/images/logo.svg"
                   className="h-8 w-auto sm:h-10"
                 />
-              </a>{" "}
+              </a>{ " " }
               <p className="ml-2 mt-1 text-xl text-blue-400 ">
                 <span>EventJacket</span>
               </p>
@@ -91,10 +94,10 @@ export default function NavBarTW() {
                 >
                   <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                     <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
-                      {solutions.map((item) => (
+                      { solutions.map( ( item ) => (
                         <a
-                          key={item.name}
-                          href={item.href}
+                          key={ item.name }
+                          href={ item.href }
                           className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
                         >
                           <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-md bg-blue-600 text-white sm:h-12 sm:w-12">
@@ -102,28 +105,28 @@ export default function NavBarTW() {
                           </div>
                           <div className="ml-4">
                             <p className="text-base font-medium text-gray-900">
-                              {item.name}
+                              { item.name }
                             </p>
                             <p className="mt-1 text-sm text-gray-500">
-                              {item.description}
+                              { item.description }
                             </p>
                           </div>
                         </a>
-                      ))}
+                      ) ) }
                     </div>
                   </div>
                 </PopoverPanel>
               </Popover>
 
-              {navigation.map((item) => (
+              { navigation.map( ( item ) => (
                 <a
-                  key={item.name}
-                  href={item.href}
+                  key={ item.name }
+                  href={ item.href }
                   className="text-base font-medium text-gray-500 hover:text-gray-900"
                 >
-                  {item.name}
+                  { item.name }
                 </a>
-              ))}
+              ) ) }
             </PopoverGroup>
 
             <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
@@ -166,34 +169,34 @@ export default function NavBarTW() {
                 </div>
                 <div className="mt-6">
                   <nav className="grid grid-cols-1 gap-7">
-                    {solutions.map((item) => (
+                    { solutions.map( ( item ) => (
                       <a
-                        key={item.name}
-                        href={item.href}
+                        key={ item.name }
+                        href={ item.href }
                         className="-m-3 flex items-center rounded-lg p-3 hover:bg-gray-50"
                       >
                         <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-md bg-blue-600 text-white">
                           <item.icon aria-hidden="true" className="h-6 w-6" />
                         </div>
                         <div className="ml-4 text-base font-medium text-gray-900">
-                          {item.name}
+                          { item.name }
                         </div>
                       </a>
-                    ))}
+                    ) ) }
                   </nav>
                 </div>
               </div>
               <div className="px-5 py-6">
                 <div className="grid grid-cols-2 gap-4">
-                  {navigation.map((item) => (
+                  { navigation.map( ( item ) => (
                     <a
-                      key={item.name}
-                      href={item.href}
+                      key={ item.name }
+                      href={ item.href }
                       className="text-base font-medium text-gray-900 hover:text-gray-700"
                     >
-                      {item.name}
+                      { item.name }
                     </a>
-                  ))}
+                  ) ) }
                 </div>
                 <div className="mt-6">
                   <a
@@ -217,7 +220,7 @@ export default function NavBarTW() {
 
       <main>
         <div>
-          {/* Hero card */}
+          {/* Hero card */ }
           <div className="relative">
             <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
             <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -262,7 +265,7 @@ export default function NavBarTW() {
             </div>
           </div>
 
-          {/* Logo cloud */}
+          {/* Logo cloud */ }
           <div className="bg-gray-100">
             <div className="mx-auto max-w-7xl px-6 py-16 lg:px-8">
               <p className="text-center text-base font-semibold text-gray-500">
@@ -289,7 +292,7 @@ export default function NavBarTW() {
           </div>
         </div>
 
-        {/* More main page content here... */}
+        {/* More main page content here... */ }
       </main>
     </div>
   );
