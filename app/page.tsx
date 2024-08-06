d57936864f679fc717d8a7ad070bf608e54b8768
Casey Spaulding <cspau17@wgu.edu>
2024-08-06 13:05:35 -0400

    updated navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 
+import NavBar1 from "@/components/NavBarTW/NavBar1";
 import CTAwPicture from "@components/CTA/CTAwPicture";
 import FeaturesGridWithIcons from "@components/Features/FeaturesGridwithIcons";
 import FooterFull from "@components/Footers/FooterFull";
@@ -7,6 +8,7 @@ import NavBarTW from "@components/NavBarTW/NavBarTW";
 export default async function Index() {
   return (
     <>
+      <NavBar1 /> 
       <NavBarTW />
       <FeaturesGridWithIcons />
       
