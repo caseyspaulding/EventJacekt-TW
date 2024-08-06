31a49f1c150d4676375713ae7712f9c373d0a17f
Casey Spaulding <cspau17@wgu.edu>
2024-08-06 07:53:29 -0400

    fixed sign up page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,27 +7,33 @@ import { Card, Checkbox, Label, TextInput } from "flowbite-react";
 import Image from "next/image";
 import Link from "next/link";
 
-interface SearchParams {
+interface SearchParams
+{
   message?: string;
 }
 
-interface SignUpProps {
+interface SignUpProps
+{
   searchParams: SearchParams;
 }
 
-export default function SignUp({ searchParams }: SignUpProps) {
+export default function SignUp ( { searchParams }: SignUpProps )
+{
   const router = useRouter();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async ( event: React.FormEvent<HTMLFormElement> ) =>
+  {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const result = await signUp(formData);
+    const formData = new FormData( event.currentTarget );
+    const result = await signUp( formData );
 
-    if (result.success && typeof result.redirectUrl === "string") {
-      router.push(result.redirectUrl);
-    } else {
+    if ( result.success && typeof result.redirectUrl === "string" )
+    {
+      router.push( result.redirectUrl );
+    } else
+    {
       // Handle error, maybe set some state to show an error message
-      console.error(result.message);
+      console.error( result.message );
       // You might want to update your UI to show this error message
     }
   };
@@ -35,22 +41,22 @@ export default function SignUp({ searchParams }: SignUpProps) {
   return (
     <>
       <div className="mx-auto flex flex-col items-center justify-center px-6 pt-8 md:h-screen">
-        
-                  <Link
-                      href="/"
-                      className="mb-8 flex items-center justify-center text-2xl font-semibold dark:text-white lg:mb-10"
-                  >
-                      <Image alt="" src="/images/logo.svg" width={ 43 } height={ 44 } className="mr-2 h-11" />
-                      <span className="self-center text-blue-600 whitespace-nowrap text-2xl font-extrabold dark:text-white">
-                          EventJacket
-                      </span>
-                  </Link>
+
+        <Link
+          href="/"
+          className="mb-8 flex items-center justify-center text-2xl font-semibold dark:text-white lg:mb-10"
+        >
+          <Image alt="" src="/images/logo.svg" width={ 43 } height={ 44 } className="mr-2 h-11" />
+          <span className="self-center text-blue-600 whitespace-nowrap text-2xl font-extrabold dark:text-white">
+            EventJacket
+          </span>
+        </Link>
         <Card
           horizontal
           imgSrc="/images/authentication/create-account.jpg"
           imgAlt=""
           className="w-full md:max-w-screen-lg"
-          theme={{
+          theme={ {
             root: {
               children: "my-auto w-full gap-0 space-y-8 p-6 sm:p-8 lg:p-16",
             },
@@ -59,12 +65,12 @@ export default function SignUp({ searchParams }: SignUpProps) {
                 on: "hidden rounded-l-lg md:w-96 md:p-0 lg:block",
               },
             },
-          }}
+          } }
         >
           <h2 className="text-2xl font-bold text-gray-900 dark:text-white lg:text-3xl">
             Create a Free Account
           </h2>
-          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+          <form onSubmit={ handleSubmit } className="mt-8 space-y-6">
             <div className="flex flex-col gap-y-2">
               <Label htmlFor="email">Your email</Label>
               <TextInput name="email" placeholder="name@company.com" required />
@@ -84,7 +90,7 @@ export default function SignUp({ searchParams }: SignUpProps) {
             </div>
 
             <div className="flex items-center gap-x-3">
-              <Checkbox id="acceptTerms" name="acceptTerms" />
+              <Checkbox name="acceptTerms" />
               <Label htmlFor="acceptTerms">
                 I accept the&nbsp;
                 <Link
@@ -103,11 +109,11 @@ export default function SignUp({ searchParams }: SignUpProps) {
               >
                 Sign Up
               </SubmitButton>
-              {searchParams?.message && (
+              { searchParams?.message && (
                 <p className="bg-foreground/10 mt-4 p-4 text-center">
-                  {searchParams.message}
+                  { searchParams.message }
                 </p>
-              )}
+              ) }
             </div>
             <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
               Already have an account?&nbsp;
