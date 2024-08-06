978060e7d8d72fc5a87f92893bb3620ce1db490b
Casey Spaulding <cspau17@wgu.edu>
2024-08-06 07:16:37 -0400

    updeted

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import CTACentered from "@/components/CTA/CTA-Centered";
 import FeaturesGridWithIcons from "@/components/Features/FeaturesGridwithIcons";
 import FooterFull from "@/components/Footers/FooterFull";
-import FooterTW from "@/components/Footers/FooterTW";
 import NavBarTW from "@/components/NavBarTW/NavBarTW";
 
 export default async function Index() {
--- components/CTA/CTA-Centered.tsx
+++ components/CTA/CTA-Centered.tsx
@@ -2,7 +2,7 @@ export default function CTACentered() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl py-20 sm:px-6 sm:py-32 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-blue-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+        <div className="relative isolate overflow-hidden bg-blue-950 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
           <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Boost your productivity today.
           </h2>
@@ -11,7 +11,7 @@ export default function CTACentered() {
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
-              href="#"
+              href="/signup"
               className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-blue-900 shadow-sm hover:bg-blue-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
               Get started
