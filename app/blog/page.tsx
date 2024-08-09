fb37783170314367a1ef6c5146032d8747b0ab05
Casey Spaulding <cspau17@wgu.edu>
2024-08-09 12:14:24 -0400

    updated blog tag line

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -17,7 +17,7 @@ export default async function BlogList ()
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
             <p className="mt-2 text-lg leading-8 text-gray-600">
-              Top Tips to Grow and Organize Stellar Events
+              Tips to Grow and Organize Stellar Events with EventJacket
             </p>
           </div>
           <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
