060f0715370bd9976ccd83cc0709e51712589647
Casey Spaulding <cspau17@wgu.edu>
2024-08-10 08:08:33 -0400

    updated qr code generator

--- components/NewQRCodeGenerator.tsx
+++ components/NewQRCodeGenerator.tsx
@@ -39,132 +39,115 @@ export default function NewQRCodeGenerator ()
   };
 
   return (
-    <><div className='app mt-20 flex justify-center items-center '>
-<h1 className='text-4xl font-extrabold'>Free QR Code Generator</h1>
-    </div><div className='app mt-20 flex justify-center items-center h-screen'>
+    <>
+      <div className='app mt-10 flex justify-center items-center'>
+        <h1 className='text-4xl font-extrabold text-center'>Free QR Code Generator</h1>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
+        <div className="w-full p-4 space-y-8">
 
-        <div>
 
-        </div>
-        <div style={ { display: 'flex', flexDirection: 'row', flexWrap: 'wrap' } }>
-          <div style={ { display: 'flex', flexDirection: 'column', flexWrap: 'wrap' } }>
-            <div style={ { display: 'flex', flexDirection: 'row', flexWrap: 'wrap' } }>
-              <div style={ { width: '240px', display: 'flex', flexDirection: 'column', padding: '15px' } }>
-                <TextArea
-                  name='value'
-                  handleChange={ handleChange } />
-                <SelectField
-                  name='ecLevel'
-                  options={ [ 'L', 'M', 'Q', 'H' ] }
-                  handleChange={ handleChange } />
-
-                <InputField
-                  name='size'
-                  type='range'
-                  handleChange={ handleChange }
-                  min={ 100 }
-                  max={ 500 } />
-                <InputField
-                  name='quietZone'
-                  type='range'
-                  handleChange={ handleChange }
-                  min={ 20 }
-                  max={ 80 } />
-                <div style={ { display: 'flex', flexDirection: 'row', marginTop: '4px', justifyContent: 'space-around' } }>
-                  <InputField
-                    name='bgColor'
-                    type='color'
-                    defaultValue='#ffffff'
-                    handleChange={ handleChange } />
-                  <InputField
-                    name='fgColor'
-                    type='color'
-                    defaultValue='#000000'
-                    handleChange={ handleChange } />
-                </div>
-              </div>
-              <div style={ { width: '240px', display: 'flex', flexDirection: 'column', padding: '15px' } }>
-                <ImageUploadField
-                  name='logoImage'
-                  handleChange={ handleChange } />
-                <InputField
-                  name='logoWidth'
-                  type='range'
-                  handleChange={ handleChange }
-                  min={ 20 }
-                  max={ 500 } />
-                <InputField
-                  name='logoHeight'
-                  type='range'
-                  handleChange={ handleChange }
-                  min={ 20 }
-                  max={ 500 } />
-                <InputField
-                  name='logoOpacity'
-                  type='range'
-                  handleChange={ handleChange }
-                  min={ 0 }
-                  max={ 1 }
-                  step={ 0.1 }
-                  defaultValue={ 1 } />
-                <SelectField
-                  name='qrStyle'
-                  options={ [ 'squares', 'dots', 'fluid' ] }
-                  handleChange={ handleChange } />
-                <CheckboxField
-                  name='removeQrCodeBehindLogo'
-                  handleChange={ handleChange } />
-                <InputField
-                  name='logoPadding'
-                  type='range'
-                  handleChange={ handleChange }
-                  min={ 0 }
-                  max={ 20 }
-                  step={ 1 }
-                  defaultValue={ 0 } />
-                <SelectField
-                  name='logoPaddingStyle'
-                  options={ [ 'square', 'circle' ] }
-                  handleChange={ handleChange } />
-              </div>
+          <div className="w-full flex justify-center p-4">
+            <div className="w-full md:w-3/4 lg:w-1/2 flex flex-col items-center border border-blue-600 rounded-3xl bg-blue-600 p-4">
+              <QRCode
+                ref={ ref as MutableRefObject<QRCode> }
+                logoOnLoad={ ( e ) => console.log( 'logo loaded', e ) }
+                { ...{
+                  ...state,
+                  eyeRadius: [
+                    {
+                      outer: [ state.eyeradius_0_outer_0, state.eyeradius_0_outer_1, state.eyeradius_0_outer_2, state.eyeradius_0_outer_3 ],
+                      inner: [ state.eyeradius_0_inner_0, state.eyeradius_0_inner_1, state.eyeradius_0_inner_2, state.eyeradius_0_inner_3 ],
+                    },
+                    {
+                      outer: [ state.eyeradius_1_outer_0, state.eyeradius_1_outer_1, state.eyeradius_1_outer_2, state.eyeradius_1_outer_3 ],
+                      inner: [ state.eyeradius_1_inner_0, state.eyeradius_1_inner_1, state.eyeradius_1_inner_2, state.eyeradius_1_inner_3 ],
+                    },
+                    {
+                      outer: [ state.eyeradius_2_outer_0, state.eyeradius_2_outer_1, state.eyeradius_2_outer_2, state.eyeradius_2_outer_3 ],
+                      inner: [ state.eyeradius_2_inner_0, state.eyeradius_2_inner_1, state.eyeradius_2_inner_2, state.eyeradius_2_inner_3 ],
+                    },
+                  ],
+                  eyeColor: [
+                    {
+                      outer: state.eyecolor_0_outer ?? state.fgColor ?? '#000000',
+                      inner: state.eyecolor_0_inner ?? state.fgColor ?? '#000000',
+                    },
+                    {
+                      outer: state.eyecolor_1_outer ?? state.fgColor ?? '#000000',
+                      inner: state.eyecolor_1_inner ?? state.fgColor ?? '#000000',
+                    },
+                    {
+                      outer: state.eyecolor_2_outer ?? state.fgColor ?? '#000000',
+                      inner: state.eyecolor_2_inner ?? state.fgColor ?? '#000000',
+                    },
+                  ],
+                } }
+              />
+              <button
+                type="button"
+                onClick={ handleDownload }
+                className="mt-5 mb-5 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+              >
+                Download QR Code
+              </button>
             </div>
-            <div style={ { padding: '15px' } }>
-              <p>eyeRadius</p>
-              <div style={ { display: 'flex', flexDirection: 'row' } }>
-                <div>
-                  <p style={ { fontSize: 14 } }>Top left eye</p>
-                  <p style={ { fontSize: 12 } }>Outer</p>
+
+          </div>
+          <div className="flex flex-col space-y-4">
+
+            <TextArea
+              name="value"
+              label="Web Address"
+              handleChange={ handleChange }
+              placeholder="Enter the web address here"
+            />
+            <SelectField name="ecLevel" options={ [ 'L', 'M', 'Q', 'H' ] } handleChange={ handleChange } />
+            <InputField name="size" type="range" handleChange={ handleChange } min={ 100 } max={ 500 } />
+            <InputField name="quietZone" type="range" handleChange={ handleChange } min={ 20 } max={ 80 } />
+            <div className="flex space-x-4">
+              <InputField name="bgColor" type="color" defaultValue="#ffffff" handleChange={ handleChange } />
+              <InputField name="fgColor" type="color" defaultValue="#000000" handleChange={ handleChange } />
+            </div>
+          </div>
+          <div className="w-full  space-y-8">
+            <div className="flex flex-col space-y-4">
+              <p className="text-lg font-semibold">Eye Radius</p>
+              <div className="flex flex-wrap gap-8">
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-medium">Top left eye</p>
+                  <p className="text-xs">Outer</p>
                   { buildEyeRadiusInput( 'eyeradius_0_outer_0' ) }
                   { buildEyeRadiusInput( 'eyeradius_0_outer_1' ) }
                   { buildEyeRadiusInput( 'eyeradius_0_outer_2' ) }
                   { buildEyeRadiusInput( 'eyeradius_0_outer_3' ) }
-                  <p style={ { fontSize: 12 } }>Inner</p>
+                  <p className="text-xs">Inner</p>
                   { buildEyeRadiusInput( 'eyeradius_0_inner_0' ) }
                   { buildEyeRadiusInput( 'eyeradius_0_inner_1' ) }
                   { buildEyeRadiusInput( 'eyeradius_0_inner_2' ) }
                   { buildEyeRadiusInput( 'eyeradius_0_inner_3' ) }
                 </div>
-                <div>
-                  <p style={ { fontSize: 14 } }>Top right eye</p>
-                  <p style={ { fontSize: 12 } }>Outer</p>
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-medium">Top right eye</p>
+                  <p className="text-xs">Outer</p>
                   { buildEyeRadiusInput( 'eyeradius_1_outer_0' ) }
                   { buildEyeRadiusInput( 'eyeradius_1_outer_1' ) }
                   { buildEyeRadiusInput( 'eyeradius_1_outer_2' ) }
                   { buildEyeRadiusInput( 'eyeradius_1_outer_3' ) }
-                  <p style={ { fontSize: 12 } }>Inner</p>
+                  <p className="text-xs">Inner</p>
                   { buildEyeRadiusInput( 'eyeradius_1_inner_0' ) }
                   { buildEyeRadiusInput( 'eyeradius_1_inner_1' ) }
                   { buildEyeRadiusInput( 'eyeradius_1_inner_2' ) }
                   { buildEyeRadiusInput( 'eyeradius_1_inner_3' ) }
                 </div>
-                <div>
-                  <p style={ { fontSize: 14 } }>Bottom left eye</p>
-                  <p style={ { fontSize: 12 } }>Outer</p>
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-medium">Bottom left eye</p>
+                  <p className="text-xs">Outer</p>
                   { buildEyeRadiusInput( 'eyeradius_2_outer_0' ) }
                   { buildEyeRadiusInput( 'eyeradius_2_outer_1' ) }
                   { buildEyeRadiusInput( 'eyeradius_2_outer_2' ) }
                   { buildEyeRadiusInput( 'eyeradius_2_outer_3' ) }
-                  <p style={ { fontSize: 12 } }>Inner</p>
+                  <p className="text-xs">Inner</p>
                   { buildEyeRadiusInput( 'eyeradius_2_inner_0' ) }
                   { buildEyeRadiusInput( 'eyeradius_2_inner_1' ) }
                   { buildEyeRadiusInput( 'eyeradius_2_inner_2' ) }
@@ -172,119 +155,94 @@ export default function NewQRCodeGenerator ()
                 </div>
               </div>
             </div>
-            <div style={ { padding: '15px' } }>
-              <p>eyeColor</p>
-              <div style={ { display: 'flex', flexDirection: 'row' } }>
-                <div>
-                  <p style={ { fontSize: 14 } }>Top left eye</p>
-                  <p style={ { fontSize: 12 } }>Outer</p>
+            <div className="flex flex-col space-y-4">
+              <p className="text-lg font-semibold">Eye Color</p>
+              <div className="flex flex-wrap gap-8">
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-medium">Top left eye</p>
+                  <p className="text-xs">Outer</p>
                   <InputField
-                    name='eyecolor_0_outer'
-                    type='color'
+                    name="eyecolor_0_outer"
+                    type="color"
                     defaultValue={ state.fgColor ?? '#000000' }
                     handleChange={ handleChange }
-                    hideLabel={ true } />
-                  <p style={ { fontSize: 12 } }>Inner</p>
+                    hideLabel={ true }
+                  />
+                  <p className="text-xs">Inner</p>
                   <InputField
-                    name='eyecolor_0_inner'
-                    type='color'
+                    name="eyecolor_0_inner"
+                    type="color"
                     defaultValue={ state.fgColor ?? '#000000' }
                     handleChange={ handleChange }
-                    hideLabel={ true } />
+                    hideLabel={ true }
+                  />
                 </div>
-                <div>
-                  <p style={ { fontSize: 14 } }>Top right eye</p>
-                  <p style={ { fontSize: 12 } }>Outer</p>
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-medium">Top right eye</p>
+                  <p className="text-xs">Outer</p>
                   <InputField
-                    name='eyecolor_1_outer'
-                    type='color'
+                    name="eyecolor_1_outer"
+                    type="color"
                     defaultValue={ state.fgColor ?? '#000000' }
                     handleChange={ handleChange }
-                    hideLabel={ true } />
-                  <p style={ { fontSize: 12 } }>Inner</p>
+                    hideLabel={ true }
+                  />
+                  <p className="text-xs">Inner</p>
                   <InputField
-                    name='eyecolor_1_inner'
-                    type='color'
+                    name="eyecolor_1_inner"
+                    type="color"
                     defaultValue={ state.fgColor ?? '#000000' }
                     handleChange={ handleChange }
-                    hideLabel={ true } />
+                    hideLabel={ true }
+                  />
                 </div>
-                <div>
-                  <p style={ { fontSize: 14 } }>Bottom left eye</p>
-                  <p style={ { fontSize: 12 } }>Outer</p>
+                <div className="flex flex-col space-y-2">
+                  <p className="text-sm font-medium">Bottom left eye</p>
+                  <p className="text-xs">Outer</p>
                   <InputField
-                    name='eyecolor_2_outer'
-                    type='color'
+                    name="eyecolor_2_outer"
+                    type="color"
                     defaultValue={ state.fgColor ?? '#000000' }
                     handleChange={ handleChange }
-                    hideLabel={ true } />
-                  <p style={ { fontSize: 12 } }>Inner</p>
+                    hideLabel={ true }
+                  />
+                  <p className="text-xs">Inner</p>
                   <InputField
-                    name='eyecolor_2_inner'
-                    type='color'
+                    name="eyecolor_2_inner"
+                    type="color"
                     defaultValue={ state.fgColor ?? '#000000' }
                     handleChange={ handleChange }
-                    hideLabel={ true } />
+                    hideLabel={ true }
+                  />
                 </div>
               </div>
             </div>
           </div>
-          <div style={ {
-            width: 750,
-            height: 750,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            border: '1px solid #155ce9',
-            borderRadius: '50px',
-            backgroundColor: '#155ce9',
-            flexDirection: 'column'
-          } }>
-            <QRCode
-              ref={ ref as MutableRefObject<QRCode> }
-              logoOnLoad={ ( e ) => console.log( 'logo loaded', e ) }
-              { ...{
-                ...state,
-                eyeRadius: [
-                  {
-                    outer: [ state.eyeradius_0_outer_0, state.eyeradius_0_outer_1, state.eyeradius_0_outer_2, state.eyeradius_0_outer_3 ],
-                    inner: [ state.eyeradius_0_inner_0, state.eyeradius_0_inner_1, state.eyeradius_0_inner_2, state.eyeradius_0_inner_3 ],
-                  },
-                  {
-                    outer: [ state.eyeradius_1_outer_0, state.eyeradius_1_outer_1, state.eyeradius_1_outer_2, state.eyeradius_1_outer_3 ],
-                    inner: [ state.eyeradius_1_inner_0, state.eyeradius_1_inner_1, state.eyeradius_1_inner_2, state.eyeradius_1_inner_3 ],
-                  },
-                  {
-                    outer: [ state.eyeradius_2_outer_0, state.eyeradius_2_outer_1, state.eyeradius_2_outer_2, state.eyeradius_2_outer_3 ],
-                    inner: [ state.eyeradius_2_inner_0, state.eyeradius_2_inner_1, state.eyeradius_2_inner_2, state.eyeradius_2_inner_3 ],
-                  }
-                ],
-                eyeColor: [
-                  {
-                    outer: state.eyecolor_0_outer ?? state.fgColor ?? '#000000',
-                    inner: state.eyecolor_0_inner ?? state.fgColor ?? '#000000'
-                  },
-                  {
-                    outer: state.eyecolor_1_outer ?? state.fgColor ?? '#000000',
-                    inner: state.eyecolor_1_inner ?? state.fgColor ?? '#000000'
-                  },
-                  {
-                    outer: state.eyecolor_2_outer ?? state.fgColor ?? '#000000',
-                    inner: state.eyecolor_2_inner ?? state.fgColor ?? '#000000'
-                  },
-                ]
-              } } />
-            <button
-              type='button'
-              onClick={ handleDownload }
-              className="mt-5 mb-5 ml-5 mr-5 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-            >
-              Download QR Code
-            </button>
-          </div>
         </div>
 
 
-      </div></>
+        <div className=''>
+
+
+          <div className='w-full  p-4'>
+            <h2 className='font-extrabold text-blue-600 text-center text-3xl'>Add Logo</h2>
+            <div className='flex flex-col space-y-4'>
+              <ImageUploadField name='logoImage' handleChange={ handleChange } />
+              <InputField name='logoWidth' type='range' handleChange={ handleChange } min={ 20 } max={ 500 } />
+              <InputField name='logoHeight' type='range' handleChange={ handleChange } min={ 20 } max={ 500 } />
+              <InputField name='logoOpacity' type='range' handleChange={ handleChange } min={ 0 } max={ 1 } step={ 0.1 } defaultValue={ 1 } />
+              <SelectField name='qrStyle' options={ [ 'squares', 'dots', 'fluid' ] } handleChange={ handleChange } />
+              <CheckboxField name='removeQrCodeBehindLogo' handleChange={ handleChange } />
+              <InputField name='logoPadding' type='range' handleChange={ handleChange } min={ 0 } max={ 20 } step={ 1 } defaultValue={ 0 } />
+              <SelectField name='logoPaddingStyle' options={ [ 'square', 'circle' ] } handleChange={ handleChange } />
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+    </>
+
   )
 }
--- components/TextArea.tsx
+++ components/TextArea.tsx
@@ -1,29 +1,34 @@
 import * as React from 'react';
 
-interface ITextAreaProps {
+interface ITextAreaProps
+{
 	name: string;
-    rows?: number;
-    cols?: number;
+	rows?: number;
+	cols?: number;
 	role?: string;
 	defaultValue?: string | number;
-	handleChange: (target: any) => void;
+	handleChange: ( target: any ) => void;
 	hideLabel?: boolean;
 	value?: string | number;
+	label?: string;
+	placeholder?: string; // New prop for placeholder text
 }
 
-export const TextArea = ({ name, handleChange, role, rows, cols, defaultValue, hideLabel, value }: ITextAreaProps) => {
+export const TextArea = ( { name, handleChange, role, rows, cols, defaultValue, hideLabel, value, label, placeholder }: ITextAreaProps ) =>
+{
 	return (
-		<div style={{ display: 'flex', flexDirection: 'column', marginBottom: '6px', whiteSpace:'pre-line' }}>
-			{!hideLabel && <label>{name}</label>}
+		<div style={ { display: 'flex', flexDirection: 'column', marginBottom: '6px', whiteSpace: 'pre-line' } }>
+			{ !hideLabel && <label htmlFor={ name }>{ label || name }</label> }
 			<textarea
-				id={name}
-				name={name}
-				onChange={handleChange}
-                rows={rows}
-                cols={cols}
-                role={role}
-				defaultValue={defaultValue}
-				value={value}
+				id={ name }
+				name={ name }
+				onChange={ handleChange }
+				rows={ rows }
+				cols={ cols }
+				role={ role }
+				defaultValue={ defaultValue }
+				value={ value }
+				placeholder={ placeholder } // Apply placeholder here
 			/>
 		</div>
 	);
