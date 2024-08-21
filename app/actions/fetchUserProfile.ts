997031b033436f198d6c71df9bf66759d11c2c63
Casey Spaulding <cspau17@wgu.edu>
2024-08-21 18:50:45 -0400

    updated fetch user profile

--- app/actions/fetchUserProfile.ts
+++ app/actions/fetchUserProfile.ts
@@ -3,42 +3,39 @@ import { db } from '@/db';
 import { userProfiles, organizations } from '@/db/schema';
 import { eq } from 'drizzle-orm/expressions';
 
-export async function fetchUserProfile() {
+export async function fetchUserProfile ()
+{
     const supabase = createClient();
     const {
         data: { user: supabaseUser }
     } = await supabase.auth.getUser();
 
-    if (!supabaseUser) {
+    if ( !supabaseUser )
+    {
         return null;
     }
 
     const userProfileData = await db
-        .select({
+        .select( {
             id: userProfiles.id,
             organizationId: userProfiles.orgId,
             organizationName: organizations.name,
 
-            stripeConnectedAccountId: userProfiles.stripeConnectedAccountId,
-            stripeCustomerId: userProfiles.stripeCustomerId,
-            stripeSubscriptionId: userProfiles.stripeSubscriptionId,
-            stripeConnectLinked: userProfiles.stripeConnectLinked
-        })
-        .from(userProfiles)
-        .innerJoin(organizations, eq(userProfiles.orgId, organizations.id))
-        .where(eq(userProfiles.userId, supabaseUser.id))
-        .limit(1);
 
-    if (userProfileData.length > 0) {
+        } )
+        .from( userProfiles )
+        .innerJoin( organizations, eq( userProfiles.orgId, organizations.id ) )
+        .where( eq( userProfiles.userId, supabaseUser.id ) )
+        .limit( 1 );
+
+    if ( userProfileData.length > 0 )
+    {
         return {
-            id: userProfileData[0].id,
+            id: userProfileData[ 0 ].id,
             email: supabaseUser.email!,
-            orgName: userProfileData[0].organizationName,
-            organizationId: userProfileData[0].organizationId,
-            stripeConnectedAccountId: userProfileData[0].stripeConnectedAccountId,
-            stripeCustomerId: userProfileData[0].stripeCustomerId,
-            stripeSubscriptionId: userProfileData[0].stripeSubscriptionId,
-            stripeConnectLinked: userProfileData[0].stripeConnectLinked
+            orgName: userProfileData[ 0 ].organizationName,
+            organizationId: userProfileData[ 0 ].organizationId,
+
         };
     }
 
