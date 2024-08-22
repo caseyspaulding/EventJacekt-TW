991435b75efb02f660cdfdda024e9635cad014b4
Casey Spaulding <cspau17@wgu.edu>
2024-08-22 11:36:19 -0400

    updated confirmed page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -4,7 +4,7 @@ export default function ThankYouPage ()
 {
   return (
     <>
-      <div className="flex h-screen min-h-screen flex-col items-center justify-center bg-gradient-to-r from-blue-500 via-sky-500 to-blue-500 px-4">
+      <div className="flex h-screen min-h-screen flex-col items-center justify-center bg-gradient-to-r from-blue-500 via-sky-500 to-indigo-600 px-4">
         <main className="flex w-full max-w-7xl flex-grow flex-col justify-center px-4 lg:px-8">
           <div className="flex flex-shrink-0 justify-center mb-8">
             <a href="#" className="inline-flex">
@@ -12,16 +12,16 @@ export default function ThankYouPage ()
               <img
                 alt="EventJacket Logo"
                 src="/images/logo.svg"
-                className="h-12 w-auto"
+                className="h-16 w-auto"
               />
             </a>
           </div>
           <div className="flex items-center justify-center">
-            <div className=" rounded-2xl shadow-2xl bg-gradient-to-r from-blue-500 via-sky-500 to-blue-500 max-w-md w-full">
-              <div className="flex flex-col rounded-2xl items-center p-6 space-y-4 bg-white">
+            <div className="rounded-2xl shadow-2xl bg-gradient-to-r from-blue-500 via-sky-500 to-blue-500 max-w-md w-full">
+              <div className="flex flex-col rounded-2xl items-center p-8 space-y-6 bg-white">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="text-green-400 w-20 h-20 sm:w-28 sm:h-28"
+                  className="text-green-400 w-24 h-24 sm:w-28 sm:h-28"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -33,16 +33,16 @@ export default function ThankYouPage ()
                     d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
-                <h1 className="text-2xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-blue-500 text-center">
+                <h1 className="text-3xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-blue-500 text-center">
                   Confirmed!
                 </h1>
-                <p className="text-center mb-3 text-sm sm:text-base">
+                <p className="text-center text-lg sm:text-base text-gray-700">
                   Thank you for confirming your email! You are all set to start
                   creating events!
                 </p>
                 <Link href="/" passHref>
-                  <div className="mt-3 inline-flex items-center px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring">
-                    <span className="text-sm font-medium">Get Started</span>
+                  <div className="mt-3 inline-flex items-center px-6 py-3 text-white bg-blue-600 rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-400">
+                    Get Started
                   </div>
                 </Link>
               </div>
