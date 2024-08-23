fc41f536a8e36316b74209c1493eb61a97b8167c
Casey Spaulding <cspau17@wgu.edu>
2024-08-23 19:08:35 -0400

    added profile page

--- app/dashboard/[org]/profile/page.tsx
+++ app/dashboard/[org]/profile/page.tsx
@@ -2,18 +2,16 @@
 
 
 import { fetchUserProfile } from '@/app/actions/fetchUserProfile'; // Fetch user profile action
-import { useUser } from '@/contexts/UserContext'; // User context hook
+
 import { redirect } from 'next/navigation';
+import UserProfileHeader from '../components/UserProfileHeader';
 
-export  default async function ProfilePage ( { params }: { params: { orgName: string } } )
+export  default async function ProfilePage (  )
 {
-  const { orgName } = params;
-  const { user } = useUser(); // Fetch the user data from the context
+  
+  
 
-  if ( !user )
-  {
-    return <div>Loading...</div>; // Or redirect to login
-  }
+  
 
   const userProfile = await fetchUserProfile();
 
@@ -24,17 +22,17 @@ export  default async function ProfilePage ( { params }: { params: { orgName: st
     return null; // You might want to return null or a loading state here
   }
 
-
+  
   return (
     <div>
-      <h1 className="text-2xl font-bold">Profile Page</h1>
-      <p className="mt-2 text-sm text-gray-500">
-        Welcome to the profile page for { user.email } in organization { orgName }.
-      </p>
-      {/* Render more profile details here */ }
-      <p>Email: { userProfile.email }</p>
-      <p>Role: { userProfile.role }</p>
-      {/* Add more user details here */ }
+      <UserProfileHeader
+        name={ userProfile.email }
+        avatar={ userProfile.avatar }
+        backgroundImage={ '/images/illustrations/profileBG.jpg' }
+        email={ userProfile.email }
+        
+      />
+      
     </div>
   );
 }
