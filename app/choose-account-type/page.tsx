4f20c553de9c33a8019890eb6c693aea67c10fa6
Casey Spaulding <cspau17@wgu.edu>
2024-08-24 16:05:07 -0400

    updated

--- app/choose-account-type/page.tsx
+++ app/choose-account-type/page.tsx
@@ -22,23 +22,27 @@ export default function ChooseAccountTypePage ()
 
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-gray-100 via-slate-100 to-gray-100 px-4">
-      <div className="mt-2 flex w-full max-w-sm flex-col gap-4 rounded-lg bg-content1 px-8 py-6 shadow-2xl text-center">
-        <h1 className="text-2xl font-bold text-blue-600">Choose Account Type</h1>
-        <p className="text-sm text-gray-800">Are you looking to attend events or create events?</p>
+      <div className="mt-2 flex w-full max-w-sm flex-col gap-4 rounded-lg bg-content1 border-blue-300 border-1 px-8 py-6 shadow-2xl text-center">
+        <img src="/images/logo.svg" alt="EventJacket" className="h-12 w-auto mx-auto" />
+        <h1 className="text-2xl font-bold text-blue-700">Choose Account Type</h1>
+        <p className="text-sm text-gray-800">Attend events or create events?</p>
         <div className="flex flex-row justify-center gap-11 m-6 ">
           <Button
             onClick={ handleAttendeeRegistration }
-            className="flex flex-col items-center justify-center w-25 h-25 bg-orange-500 hover:bg-green-600 text-white shadow-lg"
+            className="flex flex-col items-center justify-center w-25 h-25 bg-orange-500 hover:bg-orange-600 text-white shadow-lg"
+            radius='sm'
+
           >
-            <Icon icon="mdi:account" className="text-3xl" />
-            <span className="mt-1 px-1 text-sm">Attend Events</span> {/* Centered text below the icon */ }
+
+            <span className="my-8  px-1 font-medium text-sm">Attend Events</span> {/* Centered text below the icon */ }
           </Button>
           <Button
             onClick={ handleOrganizationRegistration }
-            className="flex flex-col items-center justify-center w-25 h-25 bg-orange-500 hover:bg-green-600 text-white shadow-lg"
+            className="flex flex-col items-center justify-center w-25 h-25 bg-orange-500 hover:bg-orange-600 text-white shadow-lg"
+            radius='sm'
           >
-            <Icon icon="mdi:office-building" className="text-3xl" />
-            <span className="  text-sm">Create Events </span> {/* Centered text below the icon */ }
+
+            <span className="px-1 font-medium  text-sm">Create Events </span> {/* Centered text below the icon */ }
 
           </Button>
         </div>
