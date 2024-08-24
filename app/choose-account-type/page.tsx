0d81e7ac434ca622104a9c36788f221de3560156
Casey Spaulding <cspau17@wgu.edu>
2024-08-24 15:54:01 -0400

    updated

--- app/choose-account-type/page.tsx
+++ app/choose-account-type/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@nextui-org/react';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 export default function ChooseAccountTypePage ()
 {
@@ -20,22 +21,25 @@ export default function ChooseAccountTypePage ()
   };
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-blue-600 via-sky-400 to-blue-600 px-4">
-      <div className="mt-2 flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 py-6 shadow-small text-center">
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-gray-100 via-slate-100 to-gray-100 px-4">
+      <div className="mt-2 flex w-full max-w-sm flex-col gap-4 rounded-lg bg-content1 px-8 py-6 shadow-2xl text-center">
         <h1 className="text-2xl font-bold text-blue-600">Choose Account Type</h1>
-        <p className="text-sm text-gray-800">Please select the type of account you want to create:</p>
-        <div className="flex flex-col gap-4 mt-6">
+        <p className="text-sm text-gray-800">Are you looking to attend events or create events?</p>
+        <div className="flex flex-row justify-center gap-11 m-6 ">
           <Button
             onClick={ handleAttendeeRegistration }
-            className="bg-green-500 hover:bg-green-600 text-white"
+            className="flex flex-col items-center justify-center w-25 h-25 bg-orange-500 hover:bg-green-600 text-white shadow-lg"
           >
-            Register as Attendee
+            <Icon icon="mdi:account" className="text-3xl" />
+            <span className="mt-1 px-1 text-sm">Attend Events</span> {/* Centered text below the icon */ }
           </Button>
           <Button
             onClick={ handleOrganizationRegistration }
-            className="bg-blue-500 hover:bg-blue-600 text-white"
+            className="flex flex-col items-center justify-center w-25 h-25 bg-orange-500 hover:bg-green-600 text-white shadow-lg"
           >
-            Register as Organization
+            <Icon icon="mdi:office-building" className="text-3xl" />
+            <span className="  text-sm">Create Events </span> {/* Centered text below the icon */ }
+
           </Button>
         </div>
       </div>
