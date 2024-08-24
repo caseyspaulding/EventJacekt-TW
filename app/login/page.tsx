01bd81c01b97144a440369abc195e4e405345459
Casey Spaulding <cspau17@wgu.edu>
2024-08-24 14:36:16 -0400

    updated login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Button, Input, Checkbox, Link, Divider } from "@nextui-org/react";
+import {  Input, Checkbox, Link } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
 import { signIn } from './signin'; // Adjust the path as necessary
 import { SubmitButton } from './submit-button';
@@ -10,6 +10,7 @@ import Head from 'next/head';
 
 import OneTapComponent from "@/components/GoogleOneTap";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const supabase = createClient();
 interface SearchParams
 {
@@ -54,15 +55,16 @@ export default function Component ( { searchParams }: { searchParams: SearchPara
                                 className="h-12 w-auto mx-auto"
                             />
                             <h2 className="mt-2 text-2xl font-bold leading-9 tracking-tight text-gray-900 text-center">
-                                Create an account
+                               Welcome Back  
                             </h2>
                             <p className="mt-2 text-sm leading-6 text-gray-500 text-center">
-                                Sign up to get started with EventJacket
+                                Log in to your account to continue
                             </p>
                         </div>
 
-                        <form className="space-y-6" onSubmit={ handleSubmit }>
+                        <form className="flex flex-col gap-3" action={ signIn }>
                             <Input
+
                                 label="Email Address"
                                 name="email"
                                 placeholder="Enter your email"
@@ -73,13 +75,18 @@ export default function Component ( { searchParams }: { searchParams: SearchPara
                                 required
                             />
                             <Input
-                                className="text-gray-500"
                                 endContent={
                                     <button type="button" onClick={ toggleVisibility }>
                                         { isVisible ? (
-                                            <Icon className="pointer-events-none text-2xl text-default-400" icon="solar:eye-closed-linear" />
+                                            <Icon
+                                                className="pointer-events-none text-2xl text-default-400"
+                                                icon="solar:eye-closed-linear"
+                                            />
                                         ) : (
-                                            <Icon className="pointer-events-none text-2xl text-default-400" icon="solar:eye-bold" />
+                                            <Icon
+                                                className="pointer-events-none text-2xl text-default-400"
+                                                icon="solar:eye-bold"
+                                            />
                                         ) }
                                     </button>
                                 }
@@ -92,15 +99,31 @@ export default function Component ( { searchParams }: { searchParams: SearchPara
                                 onChange={ ( e ) => setPassword( e.target.value ) }
                                 required
                             />
+                            <div className="flex items-center justify-between px-1 py-2">
+                                <Checkbox name="remember" size="sm">
+                                    Remember me
+                                </Checkbox>
+                                <Link className="text-default-500" href="#" size="sm">
+                                    Forgot password?
+                                </Link>
+                            </div>
                             <SubmitButton
-                                type="submit"
-                                className="w-full bg-blue-500"
-                                pendingText="Signing Up..."
-                                disabled={ !isFormValid }
+                                color="blue"
+                                formAction={ signIn }
+                                className="w-full bg-blue-700"
+                                pendingText="Signing In..."
+                                disabled={ !isValid } // Disable the button if the form is invalid
                             >
-                                Sign Up
+                                Log In
                             </SubmitButton>
+
+                            { searchParams?.message && (
+                                <p className="mt-4 rounded border border-red-500 bg-red-100 p-4 text-center text-red-700">
+                                    { searchParams.message }
+                                </p>
+                            ) }
                         </form>
+                        <OneTapComponent />
                         <div className="relative mt-6">
                             <div aria-hidden="true" className="absolute inset-0 flex items-center">
                                 <div className="w-full border-t border-gray-200" />
@@ -130,10 +153,10 @@ export default function Component ( { searchParams }: { searchParams: SearchPara
                             data-size="large"
                             data-logo_alignment="left"
                         ></div>
-                        <p className="text-center text-sm mt-4">
-                            Already have an account?&nbsp;
-                            <Link href="/login" size="sm">
-                                Log In
+                        <p className="text-center text-small">
+                            Need to create an account?&nbsp;
+                            <Link href="/signup" size="sm">
+                                Sign Up
                             </Link>
                         </p>
                     </div>
@@ -146,118 +169,7 @@ export default function Component ( { searchParams }: { searchParams: SearchPara
                     />
                 </div>
             </div>
-            <div className="flex h-screen min-h-screen flex-col items-center justify-center bg-gradient-to-r from-gray-200 via-blue-100 to-gray-200px-4">
-
-                <div className="mt-2 flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 py-6 shadow-2xl">
-                    <div className="flex flex-col items-center pb-6">
-                        <Link href="/">
-                            <img src='/images/logo.svg' alt='EventJacket Logo' className='h-11' />
-                        </Link>
-                        <p className="text-xl text-blue-700 font-bold">Welcome Back</p>
-                        <p className="text-small text-gray-700">Log in to your account to continue</p>
-                    </div>
-                    <form className="flex flex-col gap-3" action={ signIn }>
-                        <Input
-
-                            label="Email Address"
-                            name="email"
-                            placeholder="Enter your email"
-                            type="email"
-                            variant="bordered"
-                            value={ email }
-                            onChange={ ( e ) => setEmail( e.target.value ) }
-                            required
-                        />
-                        <Input
-                            endContent={
-                                <button type="button" onClick={ toggleVisibility }>
-                                    { isVisible ? (
-                                        <Icon
-                                            className="pointer-events-none text-2xl text-default-400"
-                                            icon="solar:eye-closed-linear"
-                                        />
-                                    ) : (
-                                        <Icon
-                                            className="pointer-events-none text-2xl text-default-400"
-                                            icon="solar:eye-bold"
-                                        />
-                                    ) }
-                                </button>
-                            }
-                            label="Password"
-                            name="password"
-                            placeholder="Enter your password"
-                            type={ isVisible ? "text" : "password" }
-                            variant="bordered"
-                            value={ password }
-                            onChange={ ( e ) => setPassword( e.target.value ) }
-                            required
-                        />
-                        <div className="flex items-center justify-between px-1 py-2">
-                            <Checkbox name="remember" size="sm">
-                                Remember me
-                            </Checkbox>
-                            <Link className="text-default-500" href="#" size="sm">
-                                Forgot password?
-                            </Link>
-                        </div>
-                        <SubmitButton
-                            color="blue"
-                            formAction={ signIn }
-                            className="w-full bg-blue-700"
-                            pendingText="Signing In..."
-                            disabled={ !isValid } // Disable the button if the form is invalid
-                        >
-                            Log In
-                        </SubmitButton>
-
-                        { searchParams?.message && (
-                            <p className="mt-4 rounded border border-red-500 bg-red-100 p-4 text-center text-red-700">
-                                { searchParams.message }
-                            </p>
-                        ) }
-                    </form>
-                    <div className="flex items-center gap-4">
-                        <Divider className="flex-1" />
-                        <p className="shrink-0 text-tiny text-default-500">OR</p>
-                        <Divider className="flex-1" />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <Button
-                            startContent={ <Icon icon="flat-color-icons:google" width={ 24 } /> }
-                            variant="bordered"
-                            onPress={ async () =>
-                            {
-                                const { data, error } = await supabase.auth.signInWithOAuth( {
-                                    provider: 'google',
-                                    options: {
-                                        redirectTo: 'https://www.eventjacket.com/auth/callback', // Make sure this matches your desired callback route
-                                    },
-                                } );
-
-                                if ( error )
-                                {
-                                    console.error( 'Google Sign-In Error:', error.message );
-                                } else if ( data.url )
-                                {
-                                    // Redirect to the provided URL from Supabase
-                                    window.location.href = data.url;
-                                }
-                            } }
-                        >
-                            Continue with Google
-                        </Button>
-
-                        <OneTapComponent />
-                    </div>
-                    <p className="text-center text-small">
-                        Need to create an account?&nbsp;
-                        <Link href="/signup" size="sm">
-                            Sign Up
-                        </Link>
-                    </p>
-                </div>
-            </div>
+         
         </>
     );
 }
