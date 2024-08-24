02152cb1ae5e76912ef143196c56f9143e582521
Casey Spaulding <cspau17@wgu.edu>
2024-08-24 10:33:27 -0400

    updated calbaock

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,5 @@
-import { createClient } from '@/utils/supabase/server';
 import { NextResponse } from 'next/server';
-export const dynamic = 'force-dynamic';
+import { createClient } from '@/utils/supabase/server';
 
 export async function GET ( request: Request )
 {
@@ -16,16 +15,16 @@ export async function GET ( request: Request )
 
     const supabase = createClient();
 
-    // Attempt to exchange the authorization code for a session
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // Exchange the authorization code for a session using PKCE
     const { data: session, error: exchangeError } = await supabase.auth.exchangeCodeForSession( code );
+
     if ( exchangeError )
     {
       console.error( 'Error exchanging code for session:', exchangeError );
       return NextResponse.redirect( 'https://www.eventjacket.com/auth/sign-in?message=Auth%20error' );
     }
 
-    // Directly redirect to the onboarding page after a successful sign-in
+    // Redirect to the onboarding page after successful exchange
     return NextResponse.redirect( 'https://www.eventjacket.com/auth/onboarding' );
 
   } catch ( error )
