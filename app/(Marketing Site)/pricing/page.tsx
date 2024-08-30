2bc7b9333dc6c213da3a3c00cb2e4a988746cf2d
Casey Spaulding <cspau17@wgu.edu>
2024-08-30 14:05:46 -0400

    updated pricing page

--- app/(Marketing Site)/pricing/page.tsx
+++ app/(Marketing Site)/pricing/page.tsx
@@ -1,10 +1,10 @@
 import
-    {
+{
 
-        CheckIcon,
+    CheckIcon,
 
 
-    } from '@heroicons/react/24/outline';
+} from '@heroicons/react/24/outline';
 
 import NavBar1 from '@/components/NavBarTW/NavBar1';
 import Link from 'next/link';
@@ -82,7 +82,7 @@ export default function Example ()
                         <div>
                             <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
                                 <span className="text-gray-900">Everything you need for</span>
-                                <span className="ml-2 text-blue-600">$ FREE -- limited time</span>
+                                <span className="ml-2 text-blue-600">$ FREE - limited time</span>
                             </h1>
                             <p className="mt-5 text-xl text-gray-500">
                                 Includes every feature we offer plus unlimited projects and
@@ -93,7 +93,7 @@ export default function Example ()
                             href="/signup"
                             className="mt-8 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-5 py-3 text-base font-medium text-white hover:bg-blue-700 sm:mt-10 sm:w-auto xl:mt-0"
                         >
-                            Get started today
+                            Get Started
                         </Link>
                     </div>
                     <div className="border-t border-gray-200 pt-16 xl:grid xl:grid-cols-3 xl:gap-x-8">
@@ -184,7 +184,7 @@ export default function Example ()
                     <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
                         <div className="inline-flex rounded-md shadow">
                             <a
-                                href="#"
+                                href="/signup"
                                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-5 py-3 text-base font-medium text-white hover:bg-blue-700"
                             >
                                 Get started
