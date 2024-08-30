7cc40d151b0753036217a4fdcbb67af185e3163b
Casey Spaulding <cspau17@wgu.edu>
2024-08-30 13:20:30 -0400

    updated

--- components/NavBarTW/NavBar1.tsx
+++ components/NavBarTW/NavBar1.tsx
@@ -212,12 +212,12 @@ export default function NavBar1 ()
             <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
               { !isAuthenticated ? (
                 <>
-                  <a href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                  <a href="/login" className="whitespace-nowrap text-base font-medium text-gray-700 hover:text-gray-900">
                     Sign in
                   </a>
                   <a
                     href="/signup"
-                    className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-3xl px-4 py-2 text-base font-medium text-white shadow-sm bg-gradient-to-r hover:bg-gradient-hover-to-r"
+                    className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-3xl px-4 py-2 text-base font-medium text-white shadow-sm bg-gradient-orange-to-r hover:bg-gradient-orange-hover-to-r "
                   >
                     Sign up
                   </a>
