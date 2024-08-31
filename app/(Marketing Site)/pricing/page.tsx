30e5adcbb487445a2223532a316f8c6835ab5f36
Casey Spaulding <cspau17@wgu.edu>
2024-08-30 20:34:04 -0400

    updated works

--- app/(Marketing Site)/pricing/page.tsx
+++ app/(Marketing Site)/pricing/page.tsx
@@ -14,6 +14,8 @@ import type { Metadata } from 'next';
 import { Button } from '@nextui-org/button';
 import ComparisonTable from '@/components/ComparisonTable/ComparisonTable';
 
+import FAQ_2 from '@/components/FAQ/FAQ_2';
+
 
 export const metadata: Metadata = {
     title: 'Pricing - EventJacket',
@@ -32,43 +34,6 @@ const features = [
     'Create custom registration forms to collect attendee information.',
     'Offer a mobile-responsive event website for each event.',
     'Generate detailed reports and analytics on ticket sales and attendance.'
-];
-const faqs = [
-    {
-        id: 1,
-        question: 'How will I get paid?',
-        answer: 'We pay you directly to your bank account!  United States, Canada, and Australia.'
-    },
-    {
-        id: 2,
-        question: 'Who processes my payments?',
-        answer: "We securely handle and process your ticket sales payments using Stripe, a leading and trusted payment processor. With Stripe, your transactions are protected by advanced security measures, ensuring your money is safe and your customers' data is secure. Plus, you can enjoy a seamless payment experience without the added fees of other processors. Rest assured, you're in good hands!"
-    },
-    {
-        id: 3,
-        question: 'Can I customize the registration form for my event?',
-        answer: 'Yes, EventJacket allows you to create custom registration forms. You can add fields to collect specific information from attendees, such as dietary requirements, session preferences, or any other details relevant to your event.'
-    },
-    {
-        id: 4,
-        question: 'How does the check-in process work with EventJacket?',
-        answer: 'EventJacket provides a streamlined check-in process using QR codes. Attendees receive a unique QR code with their ticket, which can be scanned using our mobile app or a compatible device at the event entrance for quick and efficient check-in.'
-    },
-    {
-        id: 5,
-        question: 'How secure are my payments?',
-        answer: "EventJacket is powered by Stripe with Stripe Connect, an enterprise-level payment platform that has achieved PCI-DSS 3.2.1 Level 1 certification — the highest standard of data security recognized in the payment industry! This means your financial information and your attendees' payment details are kept exceptionally secure. Rest assured, you can trust us to protect your data."
-    },
-    {
-        id: 6,
-        question: 'Is it possible to manage multiple events simultaneously on EventJacket?',
-        answer: 'Yes, EventJacket is designed to handle multiple events concurrently. You can create, manage, and track numerous events from a single dashboard, making it ideal for event planners, organizations, or venues that host various events.'
-    },
-    {
-        id: 7,
-        question: 'What\'s the maximum fee my ticket buyers will incur?',
-        answer: 'At EventJacket, our standard ticketing fee is 50 cents per ticket. Ensuring that even your higher-priced tickets remain affordable for your attendees. Please note, the 3% credit card processing fee that all ticket buyers incur is separate from the ticketing fee.'
-    }
 ];
 
 function classNames ( ...classes: string[] )
@@ -169,23 +134,10 @@ export default function Example ()
             </div>
 
             {/* Branded FAQ */ }
-            <div className="bg-blue-700">
-                <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
-                    <h2 className="text-3xl font-bold tracking-tight text-white">
-                        Frequently Asked Questions
-                    </h2>
-                    <div className="mt-6 border-t border-blue-400 border-opacity-25 pt-10">
-                        <dl className="space-y-10 md:grid md:grid-cols-2 md:grid-rows-2 md:gap-x-8 md:gap-y-12 md:space-y-0">
-                            { faqs.map( ( item ) => (
-                                <div key={ item.id }>
-                                    <dt className="text-lg font-medium leading-6 text-white">
-                                        { item.question }
-                                    </dt>
-                                    <dd className="mt-2 text-base text-blue-100">{ item.answer }</dd>
-                                </div>
-                            ) ) }
-                        </dl>
-                    </div>
+            <div className="bg-blue-600">
+                <div className="mx-auto max-w-7xl px-2 py-4 sm:py-8 lg:px-2">
+                    
+                    <FAQ_2  />
                 </div>
             </div>
 
