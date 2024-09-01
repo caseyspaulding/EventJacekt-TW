34ab4da4c0540996795fb0ef215fe51044a5b1c8
Casey Spaulding <cspau17@wgu.edu>
2024-09-01 19:24:44 -0400

    updated

--- components/Loaders/Loading.tsx
+++ components/Loaders/Loading.tsx
@@ -1,14 +1,23 @@
-// components/LoadingScreen.tsx
+import Image from 'next/image';
+
 export const LoadingScreen = () =>
 {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
       <div className="flex flex-col items-center">
-        {/* You can replace this with a GIF or spinner */ }
-        <img src="/GIFs/rocket.webp" alt="Loading" className="h-300 w-300" />
-        <p className="mt-4 text-lg font-medium text-gray-700">
-          We are building your dashboard. Please wait...
+        {/* Use Next.js Image component for optimized image loading */ }
+        <p className="mt-4 text-base sm:text-lg md:text-xl font-medium text-gray-700 text-center">
+          We are building your dashboard....
         </p>
+        <Image
+          src="/GIFs/rocket.webp"
+          alt="Loading"
+          width={ 300 }  // Replace with desired width
+          height={ 300 } // Replace with desired height
+          className="w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs"
+        />
+
+
       </div>
     </div>
   );
