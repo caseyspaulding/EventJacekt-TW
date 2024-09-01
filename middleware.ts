3c0985c04ae2aae6e9b84c5dc7ca38bc5a2d4511
Casey Spaulding <cspau17@wgu.edu>
2024-09-01 14:02:57 -0400

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,12 @@ export async function middleware ( request: NextRequest )
 {
     return updateSession( request );
 }
+
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|$|^/auth/sign-in$|^/pricing$|^/faqs$|^/signup$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|$|^/auth/sign-in$|^/pricing$|^/faqs$).*)',
         '/createpost',
         '/dashboard' // Ensure this route is protected
+
     ]
 };
