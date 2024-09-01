0c172cb4197e5f4d78b3df945b2ea90262ec6654
Casey Spaulding <cspau17@wgu.edu>
2024-09-01 14:06:52 -0400

    updated signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -123,6 +123,40 @@ export default function Component ()
                             </p>
                         </div>
 
+
+
+                        <div className='flex justify-center w-full'>
+                            <div
+                                id="g_id_onload"
+                                data-client_id="820727006892-1j07b2899mm4c8esa9ciiug6gu34ticn.apps.googleusercontent.com"
+                                data-context="signup"
+                                data-ux_mode="popup"
+                                data-callback="handleSignInWithGoogle"
+                                data-auto_select="true"
+                                data-itp_support="true"
+                                data-use_fedcm_for_prompt="true"
+                                className="flex justify-center"
+                            >
+                            </div>
+                            <div
+                                className="g_id_signin mt-4"
+                                data-type="standard"
+                                data-shape="pill"
+                                data-theme="outline"
+                                data-text="signup_with"
+                                data-size="large"
+                                data-logo_alignment="left"
+                                data-width="300"
+                            ></div>
+                        </div>
+                        <div className="relative my-6">
+                            <div aria-hidden="true" className="absolute inset-0 flex items-center">
+                                <div className="w-full border-t border-gray-200" />
+                            </div>
+                            <div className="relative flex justify-center text-sm font-medium leading-6">
+                                <span className="bg-white px-4 text-gray-900">Or continue with</span>
+                            </div>
+                        </div>
                         <form className="space-y-6" onSubmit={ handleSubmit }>
                             <Input
                                 label="Email Address"
@@ -163,38 +197,6 @@ export default function Component ()
                                 Create Account
                             </SubmitButton>
                         </form>
-                        <div className="relative mt-6">
-                            <div aria-hidden="true" className="absolute inset-0 flex items-center">
-                                <div className="w-full border-t border-gray-200" />
-                            </div>
-                            <div className="relative flex justify-center text-sm font-medium leading-6">
-                                <span className="bg-white px-4 text-gray-900">Or continue with</span>
-                            </div>
-                        </div>
-                        <div className='flex justify-center w-full'>
-                            <div
-                                id="g_id_onload"
-                                data-client_id="820727006892-1j07b2899mm4c8esa9ciiug6gu34ticn.apps.googleusercontent.com"
-                                data-context="signup"
-                                data-ux_mode="popup"
-                                data-callback="handleSignInWithGoogle"
-                                data-auto_select="true"
-                                data-itp_support="true"
-                                data-use_fedcm_for_prompt="true"
-                                className="flex justify-center"
-                            >
-                            </div>
-                            <div
-                                className="g_id_signin mt-4"
-                                data-type="standard"
-                                data-shape="pill"
-                                data-theme="outline"
-                                data-text="signin_with"
-                                data-size="large"
-                                data-logo_alignment="left"
-                                data-width="300"
-                            ></div>
-                        </div>
                         <p className="text-center text-sm mt-4">
                             Already have an account?&nbsp;
                             <Link href="/login" size="sm">
