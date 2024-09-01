c67fe27c9d8b90457f5662dc22013320a77aa91e
Casey Spaulding <cspau17@wgu.edu>
2024-09-01 13:31:29 -0400

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,20 @@
-import { type NextRequest } from 'next/server';
-import { updateSession } from './utils/supabase/middleware';
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware ( request: NextRequest )
 {
-    return updateSession( request );
+    return await updateSession( request )
 }
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|$|^/auth/sign-in$|^/pricing$|^/faqs$|^/signup$).*)',
-        '/createpost',
-        '/dashboard' // Ensure this route is protected
-    ]
-};
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * Feel free to modify this pattern to include more paths.
+         */
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    ],
+}
