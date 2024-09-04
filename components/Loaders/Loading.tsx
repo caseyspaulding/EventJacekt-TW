448e7808fbf9ed6b0d61f2b486d27298d5e340c5
Casey Spaulding <cspau17@wgu.edu>
2024-09-04 14:58:58 -0400

    updated loading screen for building dashboard

--- components/Loaders/Loading.tsx
+++ components/Loaders/Loading.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image';
 export const LoadingScreen = () =>
 {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
+    <div
+      className="flex h-screen flex-col items-center justify-center px-4 bg-cover bg-center"
+      style={ { backgroundImage: 'url(/images/illustrations/background-3.jpg)' } }
+    >
       <div className="flex flex-col items-center">
         {/* Use Next.js Image component for optimized image loading */ }
         <p className="mt-4 text-base sm:text-lg md:text-xl font-medium text-gray-700 text-center">
@@ -12,8 +15,8 @@ export const LoadingScreen = () =>
         <Image
           src="/GIFs/rocket.webp"
           alt="Loading"
-          width={ 300 }  // Replace with desired width
-          height={ 300 } // Replace with desired height
+          width={ 1200 }  // Replace with desired width
+          height={ 1200 } // Replace with desired height
           className="w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 max-w-xs"
         />
 
