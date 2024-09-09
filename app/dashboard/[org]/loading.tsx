f8d62ead32596d181f296ccfecf200c2b3d52254
Casey Spaulding <cspau17@wgu.edu>
2024-09-09 19:35:17 -0400

    added global loader

--- app/dashboard/[org]/loading.tsx
+++ app/dashboard/[org]/loading.tsx
@@ -1,7 +1,10 @@
-export default function Loading() {
+import React from 'react';
+
+export default function CustomLoader ()
+{
     return (
         <div className="flex h-screen items-center justify-center">
-            <div className="h-32 w-32 animate-spin rounded-full border-t-4 border-blue-500"></div>
+            <div className="loader"></div>
         </div>
     );
 }
---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CustomLoader ()
+{
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
+}
