ca1d571b5e42761ac82938af2744b11a4a9ed575
Casey Spaulding <cspau17@wgu.edu>
2024-09-10 11:31:23 -0400

    updated ticket quanity selector

--- app/events/[eventSlug]/TicketPurchaseClient.tsx
+++ app/events/[eventSlug]/TicketPurchaseClient.tsx
@@ -125,7 +125,7 @@ const TicketPurchaseClient = ( { ticket, eventSlug, quantity, setQuantity }: Tic
                 disabled={ loading }
                 className="w-full mt-2 rounded-3xl bg-orange-600 px-4 py-2 font-semibold text-white hover:bg-green-500"
             >
-                { loading ? 'Processing...' : 'Buy Ticket' }
+                { loading ? 'Processing...' : 'Check out' }
             </Button>
         </div>
     );
