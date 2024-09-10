03b42eeea17b59bb9b5514999b6eda3840b3839c
Casey Spaulding <cspau17@wgu.edu>
2024-09-10 13:35:40 -0400

    updated checkout

--- app/events/[eventSlug]/TicketPurchaseClient.tsx
+++ app/events/[eventSlug]/TicketPurchaseClient.tsx
@@ -123,7 +123,7 @@ const TicketPurchaseClient = ( { ticket, eventSlug, quantity, setQuantity }: Tic
             <Button
                 onClick={ handleBuyTicket }
                 disabled={ loading }
-                className="w-full mt-2 rounded-3xl text-xl  bg-orange-600 px-4 py-2 font-semibold text-white hover:bg-green-500"
+                className="w-full mt-2 rounded-3xl text-xl  bg-orange-600 px-4 py-2 font-normal text-white hover:bg-green-500"
             >
                 { loading ? 'Processing...' : 'Check out' }
             </Button>
