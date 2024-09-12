43869692622550ba3235a649392c3256c8501163
Casey Spaulding <cspau17@wgu.edu>
2024-09-12 17:14:00 -0400

    updated

--- components/EventListComponent.tsx
+++ components/EventListComponent.tsx
@@ -23,7 +23,7 @@ interface EventsListComponentProps
 const EventsListComponent: React.FC<EventsListComponentProps> = ( { eventList } ) =>
 {
   return (
-    <section className="py-16 mt-5 ">
+    <section className="py-16 mt-5 mx-auto max-w-7xl ">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
        
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
