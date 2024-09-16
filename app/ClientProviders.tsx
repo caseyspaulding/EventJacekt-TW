e783fb459c93354b9d9109cf760e2c024b216fe3
Casey Spaulding <cspau17@wgu.edu>
2024-09-16 08:01:08 -0400

    updated gtm

--- app/ClientProviders.tsx
+++ app/ClientProviders.tsx
@@ -6,11 +6,7 @@ import dynamic from 'next/dynamic';
 import Script from 'next/script';
 
 const DynamicToaster = dynamic( () => import( 'react-hot-toast' ).then( ( mod ) => mod.Toaster ), { ssr: false } );
-
-const DynamicProgressBar = dynamic(
-  () => import( 'next-nprogress-bar' ).then( ( mod ) => mod.AppProgressBar ),
-  { ssr: false }
-);
+const DynamicProgressBar = dynamic( () => import( 'next-nprogress-bar' ).then( ( mod ) => mod.AppProgressBar ), { ssr: false } );
 
 import { UserProvider } from '@/contexts/UserContext';
 
@@ -19,9 +15,14 @@ export default function ClientProviders ( { children }: { children: React.ReactN
   return (
     <NextUIProvider>
       {/* Google Analytics Script */ }
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-M6F4XVZM25" strategy="afterInteractive" />
       <Script id="google-analytics" strategy="afterInteractive">
         { `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtag/js?id=G-M6F4XVZM25';f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','G-M6F4XVZM25');
+                    
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -30,11 +31,9 @@ export default function ClientProviders ( { children }: { children: React.ReactN
       </Script>
 
       <DynamicToaster />
-
       <UserProvider initialUser={ null }>
         { children }
       </UserProvider>
-
       <DynamicProgressBar
         height="3px"
         color="#0053df"
