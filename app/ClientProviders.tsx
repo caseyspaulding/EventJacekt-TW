472fbd43461e4e1ae83b33b72d62158028f4cd27
Casey Spaulding <cspau17@wgu.edu>
2024-09-16 08:59:59 -0400

    lazy load added for scripts and gtm

--- app/ClientProviders.tsx
+++ app/ClientProviders.tsx
@@ -1,48 +1,47 @@
 'use client';
 
 import React from 'react';
-import { NextUIProvider } from '@nextui-org/react';
 import dynamic from 'next/dynamic';
 import Script from 'next/script';
 
 // Dynamically import components for better performance
 const DynamicToaster = dynamic( () => import( 'react-hot-toast' ).then( ( mod ) => mod.Toaster ), { ssr: false } );
 const DynamicProgressBar = dynamic( () => import( 'next-nprogress-bar' ).then( ( mod ) => mod.AppProgressBar ), { ssr: false } );
-
-import { UserProvider } from '@/contexts/UserContext';
+const DynamicNextUIProvider = dynamic( () => import( '@nextui-org/react' ).then( ( mod ) => mod.NextUIProvider ), { ssr: false } );
+const DynamicUserProvider = dynamic( () => import( '@/contexts/UserContext' ).then( ( mod ) => mod.UserProvider ), { ssr: false } );
 
 export default function ClientProviders ( { children }: { children: React.ReactNode } )
 {
   return (
-    <NextUIProvider>
+    <DynamicNextUIProvider>
       {/* Google Analytics Script - Optimized */ }
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-M6F4XVZM25"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
-      <Script id="google-analytics-inline" strategy="afterInteractive">
+      <Script id="google-analytics-inline" strategy="lazyOnload">
         { `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-M6F4XVZM25', {
-            'anonymize_ip': true, // Optional: anonymizes user IP for privacy
-            'send_page_view': false // Optional: control when to send page views
+            'anonymize_ip': true,
+            'send_page_view': false
           });
         `}
       </Script>
 
       {/* Load third-party components after page is interactive */ }
       <DynamicToaster />
-      <UserProvider initialUser={ null }>
+      <DynamicUserProvider initialUser={ null }>
         { children }
-      </UserProvider>
+      </DynamicUserProvider>
       <DynamicProgressBar
         height="3px"
         color="#0053df"
         options={ { showSpinner: false } }
         shallowRouting
       />
-    </NextUIProvider>
+    </DynamicNextUIProvider>
   );
 }
