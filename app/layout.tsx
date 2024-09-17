cc11dddf03b0a6db5d741847d5f50c73dbd2eb2d
Casey Spaulding <cspau17@wgu.edu>
2024-09-17 11:29:55 -0400

    updated logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,11 +41,12 @@ export default function RootLayout ( { children }: PropsWithChildren )
     return (
         <html lang="en" className={ `${ spaceGrotesk.className }` }>
             <head>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                            <link rel="manifest" href="/site.webmanifest"/>
+                                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+               
                 <link rel="canonical" href="https://www.eventjacket.com" />
             
                 <link rel="preload" href="https://www.googletagmanager.com/gtag/js?id=G-M6F4XVZM25" as="script"/>
