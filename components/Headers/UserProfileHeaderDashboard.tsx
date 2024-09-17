fd5d44edd2b10dfc0cddd8430579df5cdd3be006
Casey Spaulding <cspau17@wgu.edu>
2024-09-17 14:02:25 -0400

    updated

--- components/Headers/UserProfileHeaderDashboard.tsx
+++ components/Headers/UserProfileHeaderDashboard.tsx
@@ -88,14 +88,14 @@ const UserProfileHeaderDashboard: React.FC<UserProfileHeaderProps> = ( {
             <Button
               as="a"
               href={ `/dashboard/${ orgId }/events` } // Use orgId from params
-              className="w-full md:w-auto inline-flex items-center rounded-3xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-green-50 hover:ring-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              className="w-full md:w-auto inline-flex items-center rounded-3xl bg-white px-3 py-2 text-lg font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-green-50 hover:ring-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             >
               Manage Event
             </Button>
             <Button
               as="a"
               href={ `/dashboard/${ orgId }/events/new` } // Use orgId from params
-              className="w-full md:w-auto inline-flex items-center rounded-3xl bg-yellow-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+              className="w-full md:w-auto inline-flex items-center rounded-3xl bg-yellow-500 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-yellow-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
             >
               Create Event
             </Button>
