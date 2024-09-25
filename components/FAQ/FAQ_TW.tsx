5193edc4add3233e74a51775ff891917510359f7
Casey Spaulding <cspau17@wgu.edu>
2024-09-25 08:48:22 -0400

    updated

--- components/FAQ/FAQ_TW.tsx
+++ components/FAQ/FAQ_TW.tsx
@@ -45,7 +45,7 @@ export default function FAQ_TW ()
     <div className="bg-white">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
         <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
-          <h2 className="text-5xl font-bold leading-10 tracking-tight text-yellow-500">Frequently asked questions</h2>
+          <h2 className="text-5xl font-bold leading-10 tracking-tight text-blue-700">Frequently asked questions</h2>
           <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
             { faqs.map( ( faq ) => (
               <Disclosure key={ faq.question } as="div" className="pt-6">
