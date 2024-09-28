16f75381b198d496efd86bdb43b1557b5beb8bfd
Casey Spaulding <cspau17@wgu.edu>
2024-09-28 09:44:51 -0400

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
+// app/layout.tsx
 import type { PropsWithChildren } from 'react';
 import { Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import ClientProviders from './ClientProviders';
 import { ThemeProvider } from "@/providers/theme-provider";
-import { usePathname } from 'next/navigation';
-import Head from 'next/head';
 
 // Google Font Configuration
 const spaceGrotesk = Space_Grotesk( {
@@ -15,17 +14,8 @@ const spaceGrotesk = Space_Grotesk( {
 
 export default function RootLayout ( { children }: PropsWithChildren )
 {
-    const pathname = usePathname();
-
-    // Build the canonical URL dynamically based on the current route
-    const canonicalUrl = `https://eventjacket.com${ pathname === '/' ? '' : pathname }`;
-
     return (
         <html lang="en" className={ `${ spaceGrotesk.className }` }>
-            <Head>
-                {/* Canonical URL */ }
-                <link rel="canonical" href={ canonicalUrl } />
-            </Head>
             <body>
                 <ThemeProvider attribute="class" defaultTheme="light">
                     <ClientProviders>
