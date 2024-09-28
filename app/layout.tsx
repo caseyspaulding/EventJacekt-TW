da0cd3678a1be5a26ed515fb35c80ce7ed7c3117
Casey Spaulding <cspau17@wgu.edu>
2024-09-28 17:16:49 -0400

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import { Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import ClientProviders from './ClientProviders';
 import { ThemeProvider } from "@/providers/theme-provider";
-import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script';
+
 // Google Font Configuration
 const spaceGrotesk = Space_Grotesk( {
     weight: [ '400' ],
@@ -16,12 +17,21 @@ export default function RootLayout ( { children }: PropsWithChildren )
 {
     return (
         <html lang="en" className={ spaceGrotesk.className }>
-
-            <GoogleTagManager gtmId="G-M6F4XVZM25" />
-
+            
             <body>
-
-
+                {/* Google Tag Manager Script */ }
+                <Script
+                    strategy="afterInteractive"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-M6F4XVZM25"
+                />
+                <Script id="gtag-init" strategy="afterInteractive">
+                    { `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M6F4XVZM25');
+          `}
+                </Script>
                 <ThemeProvider attribute="class" defaultTheme="light">
                     <ClientProviders>
                         { children }
