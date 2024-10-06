0850e2faec3dabbd00a872b0fb208cac80566b8c
Casey Spaulding <cspau17@wgu.edu>
2024-10-06 13:13:31 -0400

    updated about

--- app/(Marketing Site)/about/page.tsx
+++ app/(Marketing Site)/about/page.tsx
@@ -2,6 +2,7 @@ import AboutHeroBig from '@/components/About/AboutHeroBig';
 import FooterFull from '@/components/Footers/FooterFull';
 import NavBar1 from '@/components/NavBarTW/NavBar1';
 import type { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
     title: 'About Us - EventJacket',
