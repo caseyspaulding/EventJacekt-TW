0254678be26327767cedf8dfac549d882e0761f1
Casey Spaulding <cspau17@wgu.edu>
2024-10-15 17:50:49 -0400

    updated scanner

--- components/QRCodeScanner/QRScanner.tsx
+++ components/QRCodeScanner/QRScanner.tsx
@@ -205,12 +205,12 @@ export default function QrCodeScanner ( { qrCodeSuccessCallback, onError }: QrCo
 
         {/* Another stack of buttons for Upload */ }
         <div className="w-full flex flex-col items-center space-y-4">
-          <Button
+          {/*<Button
             onClick={ handleButtonClick }
             className="px-4 py-2 w-full bg-blue-500 text-white rounded-3xl hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
           >
             Upload QR Code Image
-          </Button>
+          </Button>*/}
           <input
             type="file"
             ref={ fileInputRef }
